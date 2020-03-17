3b2705157ed218d00156fd27b2c64c9c97a8af68
oussema <oussemabarka9@gmail.com>
2020-03-17 23:34:30 +0100

    create arbitre repository

---
+++ src/main/java/Qatar2022/tn/repository/arbitrerepository.java
@@ -0,0 +1,9 @@
+package Qatar2022.tn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Qatar2022.tn.entities.arbitres;
+
+public interface arbitrerepository  extends JpaRepository<arbitres,Long>{
+
+}
