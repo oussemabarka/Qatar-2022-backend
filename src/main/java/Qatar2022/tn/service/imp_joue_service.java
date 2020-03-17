f58705378f08f493319e8c1179faf945f1d58d6e
oussema <oussemabarka9@gmail.com>
2020-03-17 23:16:29 +0100

    impliment joueur service

--- src/main/java/Qatar2022/tn/service/imp_joue_service.java
+++ src/main/java/Qatar2022/tn/service/imp_joue_service.java
@@ -1,5 +1,37 @@
 package Qatar2022.tn.service;
 
-public class imp_joue_service {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Qatar2022.tn.entities.joueurs;
+import Qatar2022.tn.entities.matchs;
+import Qatar2022.tn.repository.joueurrepository;
+
+@Service
+public class imp_joue_service implements joueurservice {
+
+	@Autowired
+	joueurrepository jou;
+	@Override
+public List<joueurs> findAllJoueurs(){
+		
+		return jou.findAll();
+	}
+	@Override
+	public joueurs saveJoueurs(joueurs joueur) {
+		return jou.save(joueur);
+		
+	}
+	@Override
+	public joueurs findJoueurs(long id) {
+		
+		return jou.findById(id).get();
+	}
+	@Override
+	public void delete(joueurs joueur) {
+		jou.delete(joueur);
+	}
+	
 }
