d90f27ffeaeb35130358bdf6ee74a9cf6bcef7da
oussema <oussemabarka9@gmail.com>
2020-03-17 23:30:19 +0100

    delete joueur

--- src/main/java/Qatar2022/tn/controller/joueurscontroller.java
+++ src/main/java/Qatar2022/tn/controller/joueurscontroller.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,4 +60,16 @@ if(joueur==null) {
 	joueurs updatejoueurs=jou.saveJoueurs(joueur);
 	return ResponseEntity.ok().body(updatejoueurs);
 	}
+	@DeleteMapping("/deletejoueurs/{id}")
+	 public ResponseEntity<joueurs> DeleteMatchs(@PathVariable(value = "id") Long id){
+		joueurs joueur=jou.findJoueurs(id);
+		if(joueur==null) {
+			return ResponseEntity.notFound().build();
+		}
+		jou.delete(joueur);
+		 return ResponseEntity.ok().build();	
+	}
+
+
+
 }
