a5afbc171ae9cc7055b637c5fb4179f0de2e2521
oussema <oussemabarka9@gmail.com>
2020-03-17 23:47:05 +0100

    add arbitre

--- src/main/java/Qatar2022/tn/controller/arbitrecontroller.java
+++ src/main/java/Qatar2022/tn/controller/arbitrecontroller.java
@@ -2,9 +2,13 @@ package Qatar2022.tn.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +20,16 @@ public class arbitrecontroller {
 	@Autowired
 	arbitreservice arb;
 	@CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping("/getjoueurs")
+    @GetMapping("/getarbitres")
 	public List<arbitres> getAllArbitres() {
 		List<arbitres> arbitre = arb.findAllArbitre();
 
         return arbitre;
 	    
 	}
+	@PostMapping("/addarbitre") 
+	public arbitres createArbitres(@Valid @RequestBody arbitres arbitre) {
+		return arb.saveArbitre(arbitre);
+		}
+	
 }
