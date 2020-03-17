5f4521ad635a8b044df039438618e897c5506a5f
oussema <oussemabarka9@gmail.com>
2020-03-17 23:48:43 +0100

    get arbitre by id

--- src/main/java/Qatar2022/tn/controller/arbitrecontroller.java
+++ src/main/java/Qatar2022/tn/controller/arbitrecontroller.java
@@ -5,8 +5,10 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,14 @@ public class arbitrecontroller {
 	public arbitres createArbitres(@Valid @RequestBody arbitres arbitre) {
 		return arb.saveArbitre(arbitre);
 		}
+	@GetMapping("/getarbitre/{id}")
+	public ResponseEntity<arbitres> getarbitre(@PathVariable(value = "id") Long id) {
+	arbitres arbitre=arb.findArbitre(id);
+if(arbitre==null) {
+	return ResponseEntity.notFound().build();
+}
+        return ResponseEntity.ok().body(arbitre);
+	    
+	}
 	
 }
