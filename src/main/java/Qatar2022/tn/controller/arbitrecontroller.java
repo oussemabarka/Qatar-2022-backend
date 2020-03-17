800e894e8f2fb14bc5dbb55c74bf62765bfe669a
oussema <oussemabarka9@gmail.com>
2020-03-17 23:54:35 +0100

    delete arbitre

--- src/main/java/Qatar2022/tn/controller/arbitrecontroller.java
+++ src/main/java/Qatar2022/tn/controller/arbitrecontroller.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,5 +58,14 @@ arbitres updatearbitre=arb.saveArbitre(arbitre);
 return ResponseEntity.ok().body(updatearbitre);
 
 	}
-	
+	@DeleteMapping("/deletearbitre/{id}")
+	 public ResponseEntity<arbitres> DeleteArbitres(@PathVariable(value = "id") Long id){
+			arbitres arbitre=arb.findArbitre(id);
+		if(arbitre==null) {
+			return ResponseEntity.notFound().build();
+		}
+		arb.delete(arbitre);
+		 return ResponseEntity.ok().build();	
+	}
+
 }
