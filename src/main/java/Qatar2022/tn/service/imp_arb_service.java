e875a0e8e75c6ead2edb30b55294ec0ef8b63cf5
oussema <oussemabarka9@gmail.com>
2020-03-17 23:41:06 +0100

    impliment_arbitre service

---
+++ src/main/java/Qatar2022/tn/service/imp_arb_service.java
@@ -0,0 +1,42 @@
+package Qatar2022.tn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Qatar2022.tn.entities.arbitres;
+import Qatar2022.tn.entities.equipes;
+import Qatar2022.tn.repository.arbitrerepository;
+import Qatar2022.tn.repository.equiperepository;
+
+@Service
+public class imp_arb_service implements arbitreservice {
+	@Autowired
+	arbitrerepository arb;
+	
+
+	@Override
+	public List<arbitres> findAllArbitre() {
+		// TODO Auto-generated method stub
+		return arb.findAll();
+	}
+
+	@Override
+	public arbitres saveArbitre(arbitres arbitre) {
+		// TODO Auto-generated method stub
+		return arb.save(arbitre);
+	}
+	@Override
+	public arbitres findArbitre(long id) {
+		// TODO Auto-generated method stub
+		return arb.findById(id).get();
+	} 
+	public void delete(arbitres arbitre)
+	{
+		arb.delete(arbitre);
+		
+	}	
+
+	
+}
